8fd4feedcaa32ad9323e5726aff0386c11346414
myfCode <mayuefeng90@126.com>
2019-02-16 16:16:34 +0800

    mock

--- routers/product.js
+++ routers/product.js
@@ -1,8 +1,21 @@
 const KoaRouter = require('koa-router')
 const router = new KoaRouter()
+const Mock = require('mockjs')
 
 router.get('/', (ctx, next) => {
-    ctx.body = 'product router'
+
+    let result = Mock.mock({
+        code: '10000',
+        message: 'OK',
+        data: {
+            'list|10-20' : [
+                {
+                    'id|+1': 1
+                }
+            ]
+        }
+    })
+    ctx.body = result
 })
 
 
